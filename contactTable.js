9c95aa2e718f990f4f825ee00ef7ec645b049e3e
Vladimir <81408568+asspirinus@users.noreply.github.com>
2021-04-12 14:41:24 +0300

    Update contactTable.js

--- contactTable.js
+++ contactTable.js
@@ -1,27 +1,41 @@
-import { LightningElement, track,wire } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import getContactList from '@salesforce/apex/ContactController.getContactList';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 export default class ContactTable extends LightningElement {
 
-    @track contactsRecord;
-    searchValue = '';
+    @track contactListRecord;
+  
+    searchValue;
  
-    
     searchKeyword(event) {
         this.searchValue = event.target.value;
     }
- 
-    
     handleSearchKeyword() {
         
         if (this.searchValue !== '') {
             getContactList({
-                    searchKey: this.searchValue
+                searchKey : this.searchValue
                 })
                 .then(result => {
-                   
-                    this.contactsRecord = result;
+                    this.contactListRecord = result;
+                    return contactListRecord;
                 })
-        
+                .catch(error => {
+                   
+                    const event = new ShowToastEvent({
+                        title: 'Error',
+                        variant: 'error',
+                        message: error.body.message,
+                    });
+                    this.dispatchEvent(event);
+                    this.contactListRecord = null;
+                });
+        } else {
+            const event = new ShowToastEvent({
+                variant: 'error',
+                message: 'Search text missing...',
+            });
+            this.dispatchEvent(event);
         }
     }
 }
